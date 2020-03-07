6d4a553102c197a4c266135f02b48db920fb3af0
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-03-07 18:46:38 +0100

    Bit crushing with sample rate reduction added

--- dspbasics.cpp
+++ dspbasics.cpp
@@ -51,6 +51,17 @@ void printFileInfo(SndfileHandle sf) {
 }
 
 
+void reduceSample(float* inbuf, float* outbuf, int numFrames, int sampleRate, int reduction) {
+    // Test: hardcoded reduction = 10. No average, use first sample
+    for (int i = 0; i < numFrames-10-1  ; i++) {
+        float sample1 = inbuf[i];
+        for (int x = 0; x < 10; x++) {
+            i++;
+            *outbuf++ = sample1;
+        }
+ 
+    }
+}
 
 // Synthesis
 // =========
@@ -108,21 +119,48 @@ void createNoise(float* buf, int numFrames, float level = 0.30f)
 // =======
 // Bitcrusher
 
-void bitCrusher(float* inbuf, float* outbuf, int numFrames, int sampleRate, int resolution) {
+// Bit crush using Sample rate reduction. Reduces the sample rate (but keeps the number of samples in the buffer)
 
-// TODO: code... nothin crushed here yet
-// TODO: get a mono WAV file with some clean single note guitar tones 
-
+void bitCrusherSamplerate(float* inbuf, float* outbuf, int numFrames, int sampleRate, int reduction) {
     // Sample rate reduction
     // Read a number of samples (for example 2 if sample rate reduced to half, or 10 if reduced to a 1/10 of original rate). 
     // Calculate average of samples
-    // Write the average values to output buf for the next 2 8or 10) samples. We need to keep original sample rate
+    // Write the average values to output buf for the next 2 (or 10) samples. We need to keep original sample rate
+ 
+    // THIS VERSION does not calculate average yet. (should we?)
+    int outframes = 0;
+    float sample = 0.f;
+    // If inbuffer is smaller than reduction we will set sample which will
+    // be used filling up remaining out buffer at the end.
+    // This is an unusual case, perhaps in a DAW with buf size set to very low.
+    if (numFrames < reduction) {
+        sample = inbuf[0]; // Might change this to calculate average
+    }
+    for (int i = 0; i < numFrames - reduction+1 ; i++) {
+        //cout << "i = " << i << " outframes = " << outframes << endl;
+        sample = inbuf[i];
+        for (int x = 0; x < reduction - 1; x++) {
+            i++;
+           outbuf[outframes++] = sample;
+        }
+        outbuf[outframes++] = sample;
+    }
+    //cout << "Done, last outframe index = " << outframes - 1 << endl; 
 
+    // if odd frames left, fill with last sample until end
+    while (outframes < numFrames) {
+        //cout << "Fill last frames, outframes index = " << outframes << endl;
+        outbuf[outframes++] = sample;
+    }
+}
 
+// Bit crush using "Resolution reduction". Reduces the number of bits used for audio samples
 
+void bitCrusherResolution(float* inbuf, float* outbuf, int numFrames, int sampleRate, int resolution) {
+ 
     // Resolution reduction
     // For example 16 bit is −32768 through 32767 (but converted to -1.0 to 1.0 float)
-    // If changed to 8 bit (256):
+    // To change to 8 bit (256):
     // multiply float with 256. Divide resulting integer value with (float)256. The resoultion will only be 8 bits (-128 through 127).
  
     int bits = pow(2, resolution);
@@ -138,9 +176,9 @@ void bitCrusher(float* inbuf, float* outbuf, int numFrames, int sampleRate, int 
 // Write a float* buffer to a WAV file (mono), 16 bit PCM
 // Using the sndfile lib. For the SndfileHandle implementation, see sndfile.hh
 
-void writeBufToWavFile(string fileName, float* buf, int bufLength, int sampleRate) {
+void writeBufToWavFile(string fileName, float* buf, int bufLength, int format = SF_FORMAT_WAV | SF_FORMAT_PCM_16, int sampleRate = 44100) {
     int numChannels = 1; // mono
-    SndfileHandle outFile = SndfileHandle(fileName.c_str(), SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_16, numChannels, sampleRate);
+    SndfileHandle outFile = SndfileHandle(fileName.c_str(), SFM_WRITE, format, numChannels, sampleRate);
     outFile.write(buf, bufLength);
 }
 
@@ -155,25 +193,39 @@ int main()
 
     // Create a tone and write to WAV file
     createTone(outputBuf.data(), 440, numFrames, sampleRate);
-    writeBufToWavFile("tone.wav", outputBuf.data(), outputBuf.size(), 44100);
+    writeBufToWavFile("tone.wav", outputBuf.data(), outputBuf.size());
 
     // Create white noise and write to WAV file
     createNoise(outputBuf.data(), numFrames);
-    writeBufToWavFile("whitenoise.wav", outputBuf.data(), outputBuf.size(), 44100);
+
+    writeBufToWavFile("whitenoise.wav", outputBuf.data(), outputBuf.size());
 
     // Running "bitcrusher" on sounds from file
     // Read buffer with test sounds from mono WAV file
     SndfileHandle infile = SndfileHandle("../testdata/single-note-clean-mono.wav");
+ //   SndfileHandle infile = SndfileHandle("bitcrushed_2_times_reduction.wav");
+  
     printFileInfo(infile);
     sf_count_t frames = infile.frames();
+    int format = infile.format();
     int rate = infile.samplerate();
     vector<float> inBuf(frames, 0);
     infile.read(&inBuf[0], frames);
     vector<float> outBuf(frames);
+
+    // 
+
     int bitResolution = 2;
     string filename = "bitcrushed_" + to_string(bitResolution) + "_bits.wav";
-    bitCrusher(inBuf.data(), outBuf.data(), frames, rate, bitResolution);
-    writeBufToWavFile(filename.c_str(), outBuf.data(), outBuf.size(), rate);
+    bitCrusherResolution(inBuf.data(), outBuf.data(), frames, rate, bitResolution);
+    writeBufToWavFile(filename.c_str(), outBuf.data(), outBuf.size(), format, rate);
+
+    //
+
+    int sampleRateReduction = 20;
+    filename = "bitcrushed_" + to_string(sampleRateReduction) + "_times_reduction.wav";
+    bitCrusherSamplerate(inBuf.data(), outBuf.data(), frames, rate, sampleRateReduction);
+    writeBufToWavFile(filename.c_str(), outBuf.data(), outBuf.size(), format, rate);
     	
 	return 0;
 }
